3eb54187354fc901e142cd3320411eb43e730f50
poeticflo12 <gelaneymatthew@gmail.com>
2020-02-03 19:36:53 -0500

    Final Commit

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,5 +1,14 @@
 class Book:
-
-    def _init_(self,author="",titles=""):
+    def __init__(self, author="", title=""):
         self.author = author
-        self.title = titles
+        self.title = title
+
+    def display(self):
+        print(self.title + ", Written by " + self.author)
+
+
+book0 = Book("John Steinbeck", "Of Mice and Men")
+book1 = Book("Harper Lee", "To Kill a Mockingbird")
+book0.display()
+book1.display()
+
